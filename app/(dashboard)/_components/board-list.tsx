e2ccde92e6dc6c0457f27939f955b9cd0c49d204
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-06-03 17:26:14 -0500

    feat(empty states): add empty states

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { EmptyBoards } from './empty-boards';
+import { EmptyFavorites } from './empty-favorites';
+import { EmptySearch } from './empty-search';
+
+interface BoardListProps {
+	orgId: string;
+	query: {
+		search?: string;
+		favorites?: string;
+	};
+}
+
+export const BoardList = ({
+	orgId,
+	query
+}: BoardListProps): React.ReactNode => {
+	const data = []; // TODO: change to api acall
+
+	if (!data?.length && query.search) {
+		return <EmptySearch />;
+	}
+
+	if (!data?.length && query.favorites) {
+		return <EmptyFavorites />;
+	}
+
+	if (!data?.length) {
+		return <EmptyBoards />;
+	}
+
+	return <div>{JSON.stringify(query)}</div>;
+};
