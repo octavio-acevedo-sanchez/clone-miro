601696b325569478a7174b0fdda6ad7ea404639b
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-06-09 16:02:05 -0500

    feat(canvas state): add canvas state to control options toolbar

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,23 +1,37 @@
 'use client';
 
-import { useSelf } from '@/liveblocks.config';
+import { useState } from 'react';
 import { Info } from './info';
 import { Participants } from './participants';
 import { Toolbar } from './toolbar';
+import { CanvasMode, type CanvasState } from '@/types/canvas';
+import { useHistory, useCanRedo, useCanUndo } from '@/liveblocks.config';
 
 interface CanvasProps {
 	boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps): React.ReactNode => {
-	const info = useSelf(me => me.info);
-	console.log({ info });
+	const [canvasState, setCanvasState] = useState<CanvasState>({
+		mode: CanvasMode.None
+	});
+
+	const history = useHistory();
+	const canUndo = useCanUndo();
+	const canRedo = useCanRedo();
 
 	return (
 		<main className='h-full w-full relative bg-neutral-100 touch-none'>
 			<Info boardId={boardId} />
 			<Participants />
-			<Toolbar />
+			<Toolbar
+				canvasState={canvasState}
+				setCanvasState={setCanvasState}
+				canRedo={canRedo}
+				canUndo={canUndo}
+				undo={history.undo}
+				redo={history.redo}
+			/>
 		</main>
 	);
 };
