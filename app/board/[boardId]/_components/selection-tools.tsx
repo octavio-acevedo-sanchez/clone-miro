e33c1089919242e1ce7fb7f3aeaa7ca6c21111d8
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-06-10 11:12:57 -0500

    feat(color change & delete): add functionality to change color and delete layer

---
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { memo } from 'react';
+import type { Camera, Color } from '@/types/canvas';
+import { useMutation, useSelf } from '@/liveblocks.config';
+import { useSelectionsBounds } from '@/hooks/use-selection-bounds';
+import { ColorPicker } from './color-picker';
+import { useDeleteLayers } from '@/hooks/use-delete-layers';
+import { Button } from '@/components/ui/button';
+import { Hint } from '@/components/hint';
+import { Trash2 } from 'lucide-react';
+
+interface SelectionToolsProps {
+	camera: Camera;
+	setLastUsedColor: (color: Color) => void;
+}
+
+export const SelectionTools = memo(
+	({ camera, setLastUsedColor }: SelectionToolsProps) => {
+		const selection = useSelf(me => me.presence.selection);
+
+		const setFill = useMutation(
+			({ storage }, fill: Color) => {
+				const liveLayers = storage.get('layers');
+				setLastUsedColor(fill);
+
+				selection.forEach(id => {
+					liveLayers.get(id)?.set('fill', fill);
+				});
+			},
+			[selection, setLastUsedColor]
+		);
+
+		const deleteLayers = useDeleteLayers();
+
+		const selectionBounds = useSelectionsBounds();
+
+		if (!selectionBounds) {
+			return null;
+		}
+
+		const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+		const y = selectionBounds.y + camera.y;
+
+		return (
+			<div
+				className='absolute p-3 rounded-xl bg-white shadow-sm border flex select-none'
+				style={{
+					transform: `translate(calc(${x}px - 50%),calc(${y - 16}px - 100%))`
+				}}
+			>
+				<ColorPicker onChange={setFill} />
+				<div className='flex items-center pl-2 ml-2 border-l border-r border-neutral-200'>
+					<Hint label='Delete'>
+						<Button variant='board' size='icon' onClick={deleteLayers}>
+							<Trash2 />
+						</Button>
+					</Hint>
+				</div>
+			</div>
+		);
+	}
+);
+
+SelectionTools.displayName = 'SelectionTools';
